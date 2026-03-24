bd2c427cc2df132c61d31bd6ba81583dcba7fa12
AP <136710760+MrParindiyal@users.noreply.github.com>
2026-03-24 09:43:47 +0000

    Remove redundant slicing

--- helpers.py
+++ helpers.py
@@ -28,34 +28,34 @@ def last_complete_session(year: int, gp: int, event: str) -> tuple:
     remaining = f1.get_events_remaining()
     # event_format = year_schedule.query(f"RoundNumber == {gp}")["EventFormat"]
     gp = max(1, min(gp, remaining["RoundNumber"].iloc[-1]))  # clip round num
-    session_num = year_schedule[:].iloc[gp-1][year_schedule[:].iloc[gp-1] == event].index[0]
+    session_num = year_schedule.iloc[gp-1][year_schedule.iloc[gp-1] == event].index[0]
 
-    if year_schedule[:].iloc[0]["Session2Date"] > datetime.now(timezone.utc):
+    if year_schedule.iloc[0]["Session2Date"] > datetime.now(timezone.utc):
         # season does not seem to have started. fallback to last year
 
         year -= 1
         year_schedule = f1.get_event_schedule(year)
         round = year_schedule["RoundNumber"].iloc[-1]
-        event = year_schedule[:].iloc[round]["Session5"]
+        event = year_schedule.iloc[round]["Session5"]
 
-    elif year_schedule[:].iloc[gp-1][f"{session_num}Date"] > datetime.now(timezone.utc):
+    elif year_schedule.iloc[gp-1][f"{session_num}Date"] > datetime.now(timezone.utc):
         # this session is in future, find last occured non-practice round
 
         for S in range(1,int(session_num[-1])):
-            if year_schedule[:].iloc[gp-1][f"Session{S}Date"] > datetime.now(timezone.utc):
+            if year_schedule.iloc[gp-1][f"Session{S}Date"] > datetime.now(timezone.utc):
                 last_valid_session = S - 1
                 break
             
-        if last_valid_session == 0 or year_schedule[:].iloc[gp-1][f"Session{last_valid_session}"].startswith("Practice"):
+        if last_valid_session == 0 or year_schedule.iloc[gp-1][f"Session{last_valid_session}"].startswith("Practice"):
             # go to previous race
             event = "Race"
-            round = remaining[:].iloc[0]["RoundNumber"] - 1
+            round = remaining.iloc[0]["RoundNumber"] - 1
             if round < 1:
                 year -= 1
                 round = f1.get_event_schedule(year)["RoundNumber"].iloc[-1]
                     
         else:
-            event = year_schedule[:].iloc[gp-1][f"Session{last_valid_session}"]
+            event = year_schedule.iloc[gp-1][f"Session{last_valid_session}"]
             round = gp
 
     else:
